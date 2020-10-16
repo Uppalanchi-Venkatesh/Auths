b86958a5d774fa374cf206f4fa99e64ca71f58b8
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2020-10-16 22:50:20 +0530

    callback URL changed

--- Backend/Strategies/passport-google.js
+++ Backend/Strategies/passport-google.js
@@ -19,7 +19,7 @@ passport.deserializeUser((id , done)=>{
 var customFields = {
     clientID: '210133538573-e2n0b18mi4flkrd94eumvsbk49o5nc8s.apps.googleusercontent.com',
     clientSecret: '2AcLW_1nKVWwCJN8LJwBVvln',
-    callbackURL: '/google/callback'
+    callbackURL: 'https://logins-system.herokuapp.com/google/callback'
 }
 
 var verifyCallback = (accessToken, refreshToken, profile, done) =>{
