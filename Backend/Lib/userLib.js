25bf6312f156f8cac0bea9dd39f4f3915ed70e02
Uppalanchi-Venkatesh <uppalanchivenkatesh@gmail.com>
2021-06-07 19:22:49 +0530

    Login with google implemented

--- Backend/Lib/userLib.js
+++ Backend/Lib/userLib.js
@@ -1,34 +1,34 @@
-var model=require("../Model/userModel");
+var model = require("../Model/userModel");
 
-module.exports={
-    createUser: function(userObj){
-        var user=new model(userObj);
+module.exports = {
+    createUser: function (userObj) {
+        var user = new model(userObj);
         user.save();
     },
-    getAllUsers: function(cb){
-        model.find({},function(err,result){
-            cb(err,result);
+    getAllUsers: function (cb) {
+        model.find({}, function (err, result) {
+            cb(err, result);
         });
     },
-    getSingleItemByQuery: function(query, model, cb){
+    getSingleItemByQuery: function (query, model, cb) {
         //console.log('Getting Single item with Query '+JSON.stringify(query));
         model.findOne(query, function (err, singleItem) {
-            if(err)
-                console.log("ERROR: "+err);
+            if (err)
+                console.log("ERROR: " + err);
             cb(err, singleItem);
         });
     },
-    getItemById: function(id, model, cb){
+    getItemById: function (id, model, cb) {
         //console.log('Getting Single item with ID '+id);
         model.findById(id, function (err, singleDBItem) {
             cb(err, singleDBItem);
         });
     },
-    getItemByQuery: function(query, model, cb){
+    getItemByQuery: function (query, model, cb) {
         //console.log('Getting item with Query '+JSON.stringify(query));
         model.find(query, function (err, allDBItems) {
-            if(err)
-                console.log("ERROR: "+err);
+            if (err)
+                console.log("ERROR: " + err);
             cb(err, allDBItems);
         });
     },
--- server.js
+++ server.js
@@ -13,15 +13,15 @@ var mongoose = require('mongoose');
 var flash = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var authRoutes = require('./Backend/Routes/authRoutes');
-var errors1 = require('./Backend/Strategies/passport-google').errors1;
-var errors2 = require('./Backend/Strategies/passport-local').errors2;
-var errors3 = require('./Backend/Strategies/passport-facebook').errors3;
-var str1="",str2="",str3="";
-require('dotenv').config(); 
+var { errors1 } = require('./Backend/Strategies/passport-google');
+var { errors2 } = require('./Backend/Strategies/passport-local');
+var { errors3 } = require('./Backend/Strategies/passport-facebook');
+var str1 = "", str2 = "", str3 = "";
+require('dotenv').config();
 
 db.connect(process.env.CONNECTION_STRING, true);
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Frontend', 'views'));
 
 app.use(express.json());
@@ -30,102 +30,105 @@ app.use(cookieParser());
 app.use(flash());
 app.use(bodyParser.json());
 app.use(session({
-    secret : process.env.SESSION_SECRET,
-    resave : false,
-    saveUninitialized : true,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 
+        maxAge: 1000 * 60 * 60 * 24 * 7
     },
     //cookie: { secure: false },
-    store : new MongoStore({mongooseConnection : mongoose.connection})
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'Frontend')));
 
-app.get('/', (req,res) =>{
-    res.render('home', {title : 'Homepage'});
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Homepage' });
 });
 
-app.get('/login', checkNotAuthenticated, (req,res) =>{
-    str1="";
-    if(errors1.length>0 && errors1[0]!=="") {
-        str1=errors1[0];
-        errors1[0]="";
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    str1 = "";
+    if (errors1.length > 0 && errors1[0] !== "") {
+        str1 = errors1[0];
+        errors1[0] = "";
     }
-    if(errors2.length>0 && errors2[0]!=="") {
-        str1=errors2[0];
-        errors2[0]="";
+    if (errors2.length > 0 && errors2[0] !== "") {
+        str1 = errors2[0];
+        errors2[0] = "";
     }
-    if(errors3.length>0 && errors3[0]!=="") {
-        str1=errors3[0];
-        errors3[0]="";
+    if (errors3.length > 0 && errors3[0] !== "") {
+        str1 = errors3[0];
+        errors3[0] = "";
     }
-    res.render('login', {title : 'Login' , message : str1});
+    res.render('login', { title: 'Login', message: str1 });
 });
 
-app.get('/register', checkNotAuthenticated, (req,res) =>{
-    str3="";
-    if(str2.length>0) {
-        str3=str2;
-        str2="";
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    str3 = "";
+    if (str2.length > 0) {
+        str3 = str2;
+        str2 = "";
     }
-    res.render('register', {title : 'Register', message : str3});
+    res.render('register', { title: 'Register', message: str3 });
 });
 
-app.post('/register', (req,res) =>{
-    str2="";
-    try{
-        var query = {email : req.body.email};
-        userLib.getSingleItemByQuery(query, model, async function(err, dbUser){
-            if(dbUser) {
-                str2='This email already taken !';
-                flag=1;
+app.get('/google', (req,res) => {
+    res.render('googleLogin', { title: 'Google Login' });
+});
+
+app.post('/register', (req, res) => {
+    str2 = "";
+    try {
+        var query = { email: req.body.email };
+        userLib.getSingleItemByQuery(query, model, async function (err, dbUser) {
+            if (dbUser) {
+                str2 = 'This email already taken !';
                 return res.redirect('/register');
-            }else {
+            } else {
                 str2 = "";
                 req.body.password = await bcrypt.hashSync(req.body.password, parseInt(process.env.SALT_ROUNDS));
                 userLib.createUser(req.body);
                 return res.redirect('/login');
             }
         });
-    }catch(err) {
-        console.log("Error : "+err);
-        str2=err;
+    } catch (err) {
+        console.log("Error : " + err);
+        str2 = err;
         res.redirect('/register');
     }
 });
 
 app.use(authRoutes);
 
-app.get('/dashboard', checkAuthenticated, (req,res) =>{
-    res.render('dashboard', {title : 'Dashboard', user : req.user.username});
+app.get('/dashboard', checkAuthenticated, (req, res) => {
+    res.render('dashboard', { title: 'Dashboard', user: req.user.username });
 });
 
 app.get('/logout', function (req, res) {
     req.logout();
     req.session.destroy(function (err) {
-        if(err)
+        if (err)
             return next(err);
         req.session = null;
         res.redirect('/login');
     });
 });
 
-function checkNotAuthenticated(req,res,next){
-    if(req.isAuthenticated())
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
         return res.redirect('/dashboard');
     next();
 }
 
-function checkAuthenticated(req,res,next){
-    if(req.isAuthenticated())
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
         return next();
     res.redirect('/login');
 }
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, (req,res) =>{
+app.listen(port, (req, res) => {
     console.log(`Site Running on http://localhost:${port}`);
 });
